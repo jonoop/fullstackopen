510bfbeed4ce73708f8d1b732650e8afbd17f9a9
cyk3 <cyk3@macbook.local>
2023-11-15 23:28:13 +0800

    complete 1.1-1.5 exercises

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,27 @@
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return <div>
-    <Header course={course}/>
-    <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+    <Header course={course.name}/>
+    <Content parts={course.parts}  />
+    <Total parts={course.parts}/>
   </div>
 }
 
@@ -18,22 +29,22 @@ function Header({course}){
   return <h1>{course}</h1>
 }
 
-function Content({part1, exercises1, part2, exercises2, part3, exercises3}){
+function Content({parts}){
   return <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      {parts.map((part)=>{
+        return <Part name={part.name} exercises={part.exercises} key={part.name}/>
+      })}
+      
   </div>
 }
 
-function Part({part, exercises}){
-  return <p>{part} {exercises}</p>
+function Part({name, exercises}){
+  return <p>{name} {exercises}</p>
 }
 
-function Total({exercises1, exercises2, exercises3}){
+function Total({parts}){
   return <div>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>Number of exercises {parts.reduce((acc,cur)=>acc + cur.exercises,0)}</p>
   </div>
 }
 
